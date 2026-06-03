8db77bc82e5b3208d2ade7bbd6d54c02309ae1fb
Guilherme Candinho <candqnb@proton.me>
2026-06-03 00:40:43 -0300

    test: refactor tests for clarity and documentation

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -1,3 +1,7 @@
+/*
+  This file contains all tests related to the matrix implementation.
+*/
+
 #include "linalg/Matrix.hpp"
 
 #include <cassert>
@@ -6,55 +10,47 @@ using namespace axion::linalg;
 
 int main() {
 
-    Matrix A(2, 2, {
+    Matrix A0(2, 2, {
 			1.0, 2.0,
 			3.0, 4.0
 		});
 
-    Matrix B(2, 2, {
+    Matrix A1(2, 2, {
 			5.0, 6.0,
 			7.0, 8.0
 		});
 
-    Matrix C = A + B;
+    Matrix A2(3, 2, {
+            3.0, 2.0, 3.0,
+            5.0, 9.0, 5.0
+        });
 
-    assert(C(0, 0) == 6.0);
-    assert(C(1, 1) == 12.0);
+    Matrix A3(2, 3, {
+            9.0, 2.0,
+            8.0, 5.0,
+            4.0, 1.0
+        });
 
-    Matrix D = A * B;
+    // Tests for matrix addition
+    assert((A0 + A1)(0,0) == 6.0);
+    assert((A0 + A1)(0,1) == 8.0);
+    assert((A0 + A1)(1,0) == 10.0);
+    assert((A0 + A1)(1,1) == 12.0);
 
-    assert(D(0, 0) == 19.0);
-    assert(D(0, 1) == 22.0);
-    assert(D(1, 0) == 43.0);
-    assert(D(1, 1) == 50.0);
+    // Tests for the addition assignment operator
+    Matrix T0 = A0;
+    T0 += A1;
 
-    Matrix E = A + 5.0;
+    assert(T0(0,0) == 6.0);
+    assert(T0(0,1) == 8.0);
+    assert(T0(1,0) == 10.0);
+    assert(T0(1,1) == 12.0);
 
-    assert(E(0,0) == 6.0); 
-    assert(E(0,1) == 7.0);
-    assert(E(1,0) == 8.0);
-    assert(E(1,1) == 9.0);
-
-    Matrix F = A - 5.0;
-
-    assert(F(0,0) == -4.0); 
-    assert(F(0,1) == -3.0);
-    assert(F(1,0) == -2.0);
-    assert(F(1,1) == -1.0);
-
-    Matrix G = A * 5;
-
-    assert(G(0,0) == 5.0);
-    assert(G(0,1) == 10.0);
-    assert(G(1,0) == 15.0);
-    assert(G(1,1) == 20.0);
-
-    Matrix H = A.hadamard(B);
-
-    assert(H(0,0) == 5.0);
-    assert(H(0,1) == 12.0);
-    assert(H(1,0) == 21.0);
-    assert(H(1,1) == 32.0);
+    // Tests for matrix multiplication
+    assert((A0 * A1)(0,0) == 19.0);
+    assert((A0 * A1)(0,1) == 22.0);
+    assert((A0 * A1)(1,0) == 43.0);
+    assert((A0 * A1)(1,1) == 50.0);
     
     return 0;
 }
